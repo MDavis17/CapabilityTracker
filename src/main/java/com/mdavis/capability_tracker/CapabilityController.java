e7ddc8411c37494cb43d868f167324e80707f407
MDavis17 <madmax.davis@gmail.com>
2018-07-31 12:19:31 -0700

    API now returns hard coded sample data, which is an array of capabilities.

--- src/main/java/com/mdavis/capability_tracker/CapabilityController.java
+++ src/main/java/com/mdavis/capability_tracker/CapabilityController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CapabilityController {
 
-//    @RequestMapping("/capabilities")
-//    public Capability[] getCapabilities(@RequestParam(value="versions",defaultValue = "") String[] versions) {
-//        return new Capability("",versions,"");
-////        return (new CapabilityCreator()).genCapabilities();
-//    }
     @RequestMapping("/capabilities")
-    public Capability getCapability(@RequestParam(value="name",defaultValue = "") String name,@RequestParam(value="version",defaultValue = "0.0.0") String version,@RequestParam(value="valueThemes",defaultValue = "") String valueThemes) {
-        return new Capability(name,version,valueThemes);
+    public Capability[] getCapabilities(@RequestParam(value="versions",defaultValue = "") String[] versions) {
+//        return new Capability[] {new Capability("","0.0.0","")};
+        return (new CapabilityCreator()).genCapabilities();
     }
+
+//    @RequestMapping("/capabilities")
+//    public Capability getCapability(@RequestParam(value="name",defaultValue = "") String name,@RequestParam(value="version",defaultValue = "0.0.0") String version,@RequestParam(value="valueThemes",defaultValue = "") String valueThemes) {
+//        return new Capability(name,version,valueThemes);
+//    }
 }
